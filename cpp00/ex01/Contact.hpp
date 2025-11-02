5e3ea04da9ccac68c0b040a51fad69a0d9c9f53f
Anas Elkadiri <aelkadir@e2r4p1.1337.ma>
2025-11-02 14:54:18 +0100

    sds

--- cpp00/ex01/Contact.hpp
+++ cpp00/ex01/Contact.hpp
@@ -16,17 +16,16 @@ class Contact
 		~Contact();
 
 		std::string GetFirstName(void);
-        	std::string GetLastName(void);
-        	std::string GetPhoneNum(void);
-        	std::string GetDarkestSecret(void);
-        	std::string GetNickName(void);
+        std::string GetLastName(void);
+        std::string GetPhoneNum(void);
+        std::string GetDarkestSecret(void);
+        std::string GetNickName(void);
 
-        	void        SetFirstName(std::string first_name);
-        	void        SetLastName(std::string last_name);
-			void        SetPhoneNumber(std::string phone_number);
-        	void        SetNickName(std::string nick_name);
-        	void        SetDarkestSecret(std::string darkest_secret);	
-
+        void        SetFirstName(std::string first_name);
+        void        SetLastName(std::string last_name);
+		void        SetPhoneNumber(std::string phone_number);
+        void        SetNickName(std::string nick_name);
+        void        SetDarkestSecret(std::string darkest_secret);	
 };
 
 #endif
