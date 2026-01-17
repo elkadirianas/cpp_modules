37c1c5c59e36c9e8c627def73551ae780ba2edc5
elkadirianas <anaskd1337@gmail.com>
2026-01-17 20:24:36 +0100

    azzaaz

--- cpp04/ex02/main.cpp
+++ cpp04/ex02/main.cpp
@@ -3,7 +3,42 @@
 #include "Cat.hpp"
 
 
+
 int main()
 {
+
+    const Animal* zoo[10];
+    std::cout << "--- creating Dogs ---" << std::endl;
+    for (int i = 0; i < 10 / 2; i++) {
+        zoo[i] = new Dog();
+    }
+    std::cout << "--- creating Cats ---" << std::endl;
+    for (int i = 10 / 2; i < 10; i++) {
+        zoo[i] = new Cat();
+    }
+
+    std::cout << "--- Testing Sounds ---" << std::endl;
+    zoo[0]->makeSound();
+    zoo[10 - 1]->makeSound(); 
+
+    std::cout << "\n--- Deleting Zoo ---" << std::endl;
+    for (int i = 0; i < 10; i++) {
+        delete zoo[i];
+    }
+
+
+    Dog basic; 
     
+    {
+        std::cout << "[Scope Start] Creating copy named 'tmp'" << std::endl;
+        Dog tmp = basic;
+        std::cout << "[Scope End] Destroying 'tmp' now..." << std::endl;
+    } 
+
+    std::cout << "[Back in Main] 'basic' is still alive." << std::endl;
+    std::cout << "If Deep Copy failed, program will crash when 'basic' is destroyed below." << std::endl;
+
+
+
+    return 0;
 }
