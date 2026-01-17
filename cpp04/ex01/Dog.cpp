520e6b3fff2007c812ec498864651d05e0713f57
elkadirianas <anaskd1337@gmail.com>
2026-01-17 19:18:19 +0100

    azaza

--- cpp04/ex01/Dog.cpp
+++ cpp04/ex01/Dog.cpp
@@ -1,15 +1,18 @@
 #include "Dog.hpp"
 
-Dog::Dog(){
-    this->type = "Dog";
+Dog::Dog():Animal("dog"){
+    this->brain = new Brain();
     std::cout << "Dog Default Constructor called" << std::endl;
 }
 
 Dog::Dog(std::string type) : Animal(type) {
+    this->brain = new Brain();
     std::cout << "Dog " << type << " Constructor called" << std::endl;
 }
 
 Dog::Dog(const Dog &other) : Animal(other) {
+    brain = new Brain();
+    *brain = *other.brain ; 
     std::cout << "Dog Copy Constructor called" << std::endl;
 }
 
@@ -17,11 +20,13 @@ Dog &Dog::operator=(const Dog &other) {
     std::cout << "Dog Assignment Operator called" << std::endl;
     if (this != &other) {
         Animal::operator=(other);
+        *brain = *other.brain ; 
     }
     return *this;
 }
 
 Dog::~Dog() {
+    delete this->brain ; 
     std::cout << "Dog Destructor called" << std::endl;
 }
 
